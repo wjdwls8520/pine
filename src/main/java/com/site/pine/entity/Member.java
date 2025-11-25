e0aa222b10f83368cd49e0f2a12ad9cccc864c70
seonghee10 <seonghui862@gmail.com>
2025-11-25 20:47:00 +0900

    네이버 로그인 적용

---
+++ src/main/java/com/site/pine/entity/Member.java
@@ -0,0 +1,60 @@
+package com.site.pine.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer m_idx;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String pwd;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String nickname;
+    @Column(nullable = false)
+    private String resident_num;
+    @Column(nullable = false)
+    private String phone;
+    @Column(nullable = false)
+    private Integer level;
+    @Column(nullable = false)
+    private String address_code;
+    @Column(nullable = false)
+    private String address_1;
+    @Column(nullable = false)
+    private String address_2;
+    private String job;
+    private String profile_img;
+    private String profile_msg;
+    @Column(nullable = false)
+    private String provider;
+    @Column(nullable = false)
+    private String position;
+    @Column(nullable = false)
+    private Timestamp join_date;
+    @Column(nullable = false)
+    private Boolean terms_agreed;
+    @Column(nullable = false)
+    private Timestamp terms_agreed_date;
+    @Column(nullable = false)
+    private Boolean privacy_agreed;
+    @Column(nullable = false)
+    private Timestamp privacy_agreed_date;
+    @Column(nullable = false)
+    private Boolean marketing_agreed;
+    @Column(nullable = false)
+    private Timestamp marketing_agreed_date;
+    @Column(nullable = false)
+    private String agreed_version;
+
+}
---
+++ src/main/webapp/WEB-INF/views/member/jointerms.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="./include/head.jsp"></jsp:include>
+    <link rel="stylesheet" href="/css/home.css">
+    <link rel="stylesheet" href="/css/login.css">
+
+    <script src="/js/login.js"></script>
+</head>
+<body>
+    <jsp:include page="./include/header.jsp"></jsp:include>
+    <div class="wrap">
+        <jsp:include page="./include/sideBar.jsp"></jsp:include>
+
+        여기는 이용약관 페이지입니다. 로그인 정보 없을때만 옵니다.
+
+    </div>
+
+</body>
+</html>
