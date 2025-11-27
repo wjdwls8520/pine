b7cc2b727bfedd731c9d12da6ec511d60b0b8c50
hangeunyoung <hms09131@gmail.com>
2025-11-27 21:54:49 +0900

    25.11.27

--- src/main/java/com/site/pine/controller/ShortsRestController.java
+++
@@ -1,9 +0,0 @@
-package com.site.pine.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/shorts")
-public class ShortsRestController {
-}
