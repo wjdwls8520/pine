2ed5908968b561b963df8c24cba1ae5cecccf64b
geunyoung <hms09131@gmail.com>
2026-01-09 23:58:22 +0900

    댓글 수정 구현중/쇼츠게시물 디자인 수정

---
+++ src/main/java/com/site/pine/dto/reply/ReplyUpdateReqDto.java
@@ -0,0 +1,16 @@
+package com.site.pine.dto.reply;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+
+//@Slf4j
+//@Getter
+//@Builder
+//public class ReplyUpdateReqDto {
+//        private Long replyId;
+//        private String content;
+//        private LocalDateTime updateDate;
+//}
